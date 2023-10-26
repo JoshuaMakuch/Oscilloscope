a2645fe5e7561f101dbabfdd486d33839846700e
JoshuaMakuch <joshuamakuch9@gmail.com>
2023-10-26 06:46:09 -0600

    got volts/division working and  cleared up the form

--- Oscilloscope/OscilloscopeForm.Designer.vb
+++ Oscilloscope/OscilloscopeForm.Designer.vb
@@ -1,9 +1,9 @@
-﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
 Partial Class OscilloscopeForm
     Inherits System.Windows.Forms.Form
 
     'Form overrides dispose to clean up the component list.
-    <System.Diagnostics.DebuggerNonUserCode()> _
+    <System.Diagnostics.DebuggerNonUserCode()>
     Protected Overrides Sub Dispose(ByVal disposing As Boolean)
         Try
             If disposing AndAlso components IsNot Nothing Then
@@ -20,19 +20,187 @@ Partial Class OscilloscopeForm
     'NOTE: The following procedure is required by the Windows Form Designer
     'It can be modified using the Windows Form Designer.  
     'Do not modify it using the code editor.
-    <System.Diagnostics.DebuggerStepThrough()> _
+    <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.PBDrawing = New System.Windows.Forms.PictureBox()
+        Me.DrawButton = New System.Windows.Forms.Button()
+        Me.SelectColorButton = New System.Windows.Forms.Button()
+        Me.ExitButton = New System.Windows.Forms.Button()
+        Me.ClearButton = New System.Windows.Forms.Button()
+        Me.UpButton = New System.Windows.Forms.Button()
+        Me.DownButton = New System.Windows.Forms.Button()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.VerticalOffsetBar = New System.Windows.Forms.TrackBar()
+        Me.BackgroundWorker1 = New System.ComponentModel.BackgroundWorker()
+        Me.TimePerDivisionDomain = New System.Windows.Forms.DomainUpDown()
+        Me.OffsetTextBox = New System.Windows.Forms.TextBox()
+        Me.RNGTextBox = New System.Windows.Forms.TextBox()
+        Me.VoltsPerDivisionDomain = New System.Windows.Forms.DomainUpDown()
+        CType(Me.PBDrawing, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.VerticalOffsetBar, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
+        '
+        'PBDrawing
+        '
+        Me.PBDrawing.BackColor = System.Drawing.SystemColors.ControlText
+        Me.PBDrawing.Location = New System.Drawing.Point(74, 14)
+        Me.PBDrawing.Name = "PBDrawing"
+        Me.PBDrawing.Size = New System.Drawing.Size(1000, 500)
+        Me.PBDrawing.TabIndex = 0
+        Me.PBDrawing.TabStop = False
+        '
+        'DrawButton
+        '
+        Me.DrawButton.BackColor = System.Drawing.SystemColors.Control
+        Me.DrawButton.Location = New System.Drawing.Point(1080, 14)
+        Me.DrawButton.Name = "DrawButton"
+        Me.DrawButton.Size = New System.Drawing.Size(136, 78)
+        Me.DrawButton.TabIndex = 1
+        Me.DrawButton.Text = "Draw"
+        Me.DrawButton.UseVisualStyleBackColor = False
+        '
+        'SelectColorButton
+        '
+        Me.SelectColorButton.BackColor = System.Drawing.SystemColors.Control
+        Me.SelectColorButton.Location = New System.Drawing.Point(1080, 98)
+        Me.SelectColorButton.Name = "SelectColorButton"
+        Me.SelectColorButton.Size = New System.Drawing.Size(136, 78)
+        Me.SelectColorButton.TabIndex = 2
+        Me.SelectColorButton.Text = "Select Waveform Color"
+        Me.SelectColorButton.UseVisualStyleBackColor = False
+        '
+        'ExitButton
+        '
+        Me.ExitButton.Location = New System.Drawing.Point(1080, 567)
+        Me.ExitButton.Name = "ExitButton"
+        Me.ExitButton.Size = New System.Drawing.Size(136, 78)
+        Me.ExitButton.TabIndex = 4
+        Me.ExitButton.Text = "Exit"
+        Me.ExitButton.UseVisualStyleBackColor = True
+        '
+        'ClearButton
+        '
+        Me.ClearButton.BackColor = System.Drawing.SystemColors.Control
+        Me.ClearButton.Location = New System.Drawing.Point(1080, 182)
+        Me.ClearButton.Name = "ClearButton"
+        Me.ClearButton.Size = New System.Drawing.Size(136, 78)
+        Me.ClearButton.TabIndex = 3
+        Me.ClearButton.Text = "Clear"
+        Me.ClearButton.UseVisualStyleBackColor = False
+        '
+        'UpButton
+        '
+        Me.UpButton.Location = New System.Drawing.Point(1080, 266)
+        Me.UpButton.Name = "UpButton"
+        Me.UpButton.Size = New System.Drawing.Size(136, 78)
+        Me.UpButton.TabIndex = 5
+        Me.UpButton.Text = "Up"
+        Me.UpButton.UseVisualStyleBackColor = True
+        '
+        'DownButton
+        '
+        Me.DownButton.Location = New System.Drawing.Point(1079, 350)
+        Me.DownButton.Name = "DownButton"
+        Me.DownButton.Size = New System.Drawing.Size(136, 78)
+        Me.DownButton.TabIndex = 6
+        Me.DownButton.Text = "Down"
+        Me.DownButton.UseVisualStyleBackColor = True
+        '
+        'Timer1
+        '
+        Me.Timer1.Enabled = True
+        Me.Timer1.Interval = 1
+        '
+        'VerticalOffsetBar
+        '
+        Me.VerticalOffsetBar.BackColor = System.Drawing.Color.Gray
+        Me.VerticalOffsetBar.Cursor = System.Windows.Forms.Cursors.NoMoveVert
+        Me.VerticalOffsetBar.Location = New System.Drawing.Point(12, 5)
+        Me.VerticalOffsetBar.Name = "VerticalOffsetBar"
+        Me.VerticalOffsetBar.Orientation = System.Windows.Forms.Orientation.Vertical
+        Me.VerticalOffsetBar.Size = New System.Drawing.Size(56, 517)
+        Me.VerticalOffsetBar.TabIndex = 8
+        '
+        'TimePerDivisionDomain
+        '
+        Me.TimePerDivisionDomain.Items.Add("Time/Div: 1s")
+        Me.TimePerDivisionDomain.Items.Add("Time/Div: 100mS")
+        Me.TimePerDivisionDomain.Items.Add("Time/Div: 10mS")
+        Me.TimePerDivisionDomain.Items.Add("Time/Div: 1mS")
+        Me.TimePerDivisionDomain.Location = New System.Drawing.Point(1079, 462)
+        Me.TimePerDivisionDomain.Name = "TimePerDivisionDomain"
+        Me.TimePerDivisionDomain.Size = New System.Drawing.Size(136, 22)
+        Me.TimePerDivisionDomain.TabIndex = 9
+        Me.TimePerDivisionDomain.Text = "Time/Div: 1mS"
+        '
+        'OffsetTextBox
+        '
+        Me.OffsetTextBox.Location = New System.Drawing.Point(12, 528)
+        Me.OffsetTextBox.Name = "OffsetTextBox"
+        Me.OffsetTextBox.Size = New System.Drawing.Size(56, 22)
+        Me.OffsetTextBox.TabIndex = 10
+        '
+        'RNGTextBox
+        '
+        Me.RNGTextBox.Location = New System.Drawing.Point(1079, 434)
+        Me.RNGTextBox.Name = "RNGTextBox"
+        Me.RNGTextBox.Size = New System.Drawing.Size(136, 22)
+        Me.RNGTextBox.TabIndex = 11
+        Me.RNGTextBox.Text = "RNG Value: "
+        '
+        'VoltsPerDivisionDomain
+        '
+        Me.VoltsPerDivisionDomain.Items.Add("Volts/Div: 5V")
+        Me.VoltsPerDivisionDomain.Items.Add("Volts/Div: 2V")
+        Me.VoltsPerDivisionDomain.Items.Add("Volts/Div: 1V")
+        Me.VoltsPerDivisionDomain.Items.Add("Volts/Div: 500mV")
+        Me.VoltsPerDivisionDomain.Items.Add("Volts/div: 100mV")
+        Me.VoltsPerDivisionDomain.Location = New System.Drawing.Point(1079, 490)
+        Me.VoltsPerDivisionDomain.Name = "VoltsPerDivisionDomain"
+        Me.VoltsPerDivisionDomain.Size = New System.Drawing.Size(136, 22)
+        Me.VoltsPerDivisionDomain.TabIndex = 12
+        Me.VoltsPerDivisionDomain.Text = "Volts/Div: 1V"
         '
         'OscilloscopeForm
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.BackColor = System.Drawing.SystemColors.ControlDarkDark
+        Me.ClientSize = New System.Drawing.Size(1227, 653)
+        Me.Controls.Add(Me.VoltsPerDivisionDomain)
+        Me.Controls.Add(Me.RNGTextBox)
+        Me.Controls.Add(Me.OffsetTextBox)
+        Me.Controls.Add(Me.TimePerDivisionDomain)
+        Me.Controls.Add(Me.VerticalOffsetBar)
+        Me.Controls.Add(Me.DownButton)
+        Me.Controls.Add(Me.UpButton)
+        Me.Controls.Add(Me.ExitButton)
+        Me.Controls.Add(Me.ClearButton)
+        Me.Controls.Add(Me.SelectColorButton)
+        Me.Controls.Add(Me.DrawButton)
+        Me.Controls.Add(Me.PBDrawing)
         Me.Name = "OscilloscopeForm"
         Me.Text = "OscilloscopeForm"
+        CType(Me.PBDrawing, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.VerticalOffsetBar, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
 
+    Friend WithEvents PBDrawing As PictureBox
+    Friend WithEvents DrawButton As Button
+    Friend WithEvents SelectColorButton As Button
+    Friend WithEvents ExitButton As Button
+    Friend WithEvents ClearButton As Button
+    Friend WithEvents UpButton As Button
+    Friend WithEvents DownButton As Button
+    Friend WithEvents Timer1 As Timer
+    Friend WithEvents VerticalOffsetBar As TrackBar
+    Friend WithEvents BackgroundWorker1 As System.ComponentModel.BackgroundWorker
+    Friend WithEvents TimePerDivisionDomain As DomainUpDown
+    Friend WithEvents OffsetTextBox As TextBox
+    Friend WithEvents RNGTextBox As TextBox
+    Friend WithEvents VoltsPerDivisionDomain As DomainUpDown
 End Class
--- Oscilloscope/OscilloscopeForm.vb
+++ Oscilloscope/OscilloscopeForm.vb
@@ -1,6 +1,181 @@
-﻿
+﻿'Joshua Makuch
+'RCET 3371
+'October 17, 2023
+'Oscilloscope Form
+'https://github.com/JoshuaMakuch/Oscilliscope
 
 
 Public Class OscilloscopeForm
 
+    Dim g As Graphics = Me.CreateGraphics
+    Dim myPen As Pen = New Pen(Color.White)
+    Dim GridPen As Pen = New Pen(Color.Gray)
+    Dim bmp As Bitmap
+    Dim RNGVal As Integer
+    Dim DrawnScale As Integer
+    Dim LastX As Integer
+    Dim LastY As Integer
+    Dim NewX As Integer
+    Dim NewY As Integer
+    Dim Draw As Boolean = False
+    Dim DrawIterations As Integer
+
+    'On Form Load
+    Private Sub OscilloscopeForm_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        'Populate picture box image property
+        Draw = False
+        bmp = New Bitmap(PBDrawing.Width, PBDrawing.Height) 'Sets the bitmap to be the size of the picture box
+        PBDrawing.Image = bmp
+        PBDrawing.BackColor = Color.Black
+        DrawButton().Focus()
+        RNGVal = 25
+        RNGTextBox.Text = "RNG Value: " & CStr(RNGVal)
+        VerticalOffsetBar.Maximum = PBDrawing.Height
+        VerticalOffsetBar.Value = VerticalOffsetBar.Maximum / 2
+        OffsetTextBox.Text = $"{(VerticalOffsetBar.Value - PBDrawing.Height / 2) / 100} V"
+        TimePerDivisionDomain.SelectedIndex = 3
+        VoltsPerDivisionDomain.SelectedIndex = 2
+        VoltsPerDivisionDomain_SelectedItemChanged(sender, e)
+        TimePerDivisionDomain_SelectedItemChanged(sender, e)
+        DrawGrid()
+    End Sub
+
+    'THIS IS THE MAIN DRAWING CODE||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
+
+    'Exit Button
+    Private Sub ExitButton_Click(sender As Object, e As EventArgs) Handles ExitButton.Click
+        Me.Close()
+    End Sub
+
+    'Sets the color of the drawn line, Color Button
+    Private Sub SelectColorButton_Click(sender As Object, e As EventArgs) Handles SelectColorButton.Click
+        'Opens a window dialog to be able to change the color as desired
+        Dim myDialog As New ColorDialog()
+        If (myDialog.ShowDialog() = Windows.Forms.DialogResult.OK) Then
+            myPen.Color = myDialog.Color
+        End If
+        DrawButton().Focus()
+    End Sub
+
+    'Clear Button
+    Private Sub ClearButton_Click(sender As Object, e As EventArgs) Handles ClearButton.Click
+        bmp = New Bitmap(PBDrawing.Width, PBDrawing.Height) 'Sets the bitmap to be the size of the picture box
+        PBDrawing.Image = bmp
+        DrawButton().Focus()
+        Draw = False
+        DrawGrid()
+    End Sub
+
+    'Draw Waveform Button
+    Private Sub DrawButton_Click(sender As Object, e As EventArgs) Handles DrawButton.Click
+
+        Draw = True
+        DrawIterations = 0
+
+    End Sub
+
+    'Up Button 
+    Private Sub UpButton_Click(sender As Object, e As EventArgs) Handles UpButton.Click
+        If RNGVal < PBDrawing.Height / 2 - 25 Then RNGVal += 25 Else RNGVal = PBDrawing.Height / 2
+        RNGTextBox.Text = "RNG Value: " & CStr(RNGVal)
+        VoltsPerDivisionDomain_SelectedItemChanged(sender, e)
+    End Sub
+
+    'Down Button
+    Private Sub DownButton_Click(sender As Object, e As EventArgs) Handles DownButton.Click
+        If RNGVal > -PBDrawing.Height / 2 + 25 Then RNGVal -= 25 Else RNGVal = -PBDrawing.Height / 2
+        RNGTextBox.Text = "RNG Value: " & CStr(RNGVal)
+        VoltsPerDivisionDomain_SelectedItemChanged(sender, e)
+    End Sub
+
+    'Timer1 Tick Handle
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        If Draw Then
+            Using g As Graphics = Graphics.FromImage(PBDrawing.Image)
+                If DrawIterations < PBDrawing.Width - 1 Then
+                    g.DrawLine(New Pen(Color.Black), DrawIterations + 1, PBDrawing.Height, DrawIterations + 1, 0)
+
+                    If DrawIterations Mod Math.Round(PBDrawing.Width / 10) = 0 Then
+                        g.DrawLine(GridPen, DrawIterations, PBDrawing.Height, DrawIterations, 0)
+                    End If
+
+                    For i As Integer = 1 To 7
+                        g.DrawLine(GridPen, DrawIterations, Convert.ToSingle((Math.Round(PBDrawing.Height * i / 8))), DrawIterations + 1, Convert.ToSingle((Math.Round(PBDrawing.Height * i / 8))))
+                    Next
+
+                    g.DrawLine(New Pen(Color.Blue), DrawIterations, -VerticalOffsetBar.Value + PBDrawing.Height, DrawIterations + 1, -VerticalOffsetBar.Value + PBDrawing.Height)
+                    NewY = (Rnd() * (-DrawnScale)) + (-VerticalOffsetBar.Value + PBDrawing.Height)
+                    g.DrawLine(myPen, DrawIterations, LastY, DrawIterations + 1, NewY)
+                    LastY = NewY
+                    PBDrawing.Refresh()
+                    DrawIterations += 1
+
+                Else
+                    Draw = False
+                    DrawIterations = 0
+                End If
+            End Using
+        End If
+    End Sub
+
+    'Custom Sub DrawGridhandle
+    Sub DrawGrid()
+        bmp = New Bitmap(PBDrawing.Width, PBDrawing.Height) 'Sets the bitmap to be the size of the picture box
+        PBDrawing.Image = bmp
+        DrawButton().Focus()
+        Draw = False
+        Using g As Graphics = Graphics.FromImage(PBDrawing.Image)
+            For i As Integer = 1 To 9
+                g.DrawLine(GridPen, Convert.ToSingle((Math.Round(PBDrawing.Width * i / 10))), 0, Convert.ToSingle((Math.Round(PBDrawing.Width * i / 10))), PBDrawing.Height)
+            Next
+            For i As Integer = 1 To 7
+                g.DrawLine(GridPen, 0, Convert.ToSingle(Math.Round(i * PBDrawing.Height / 8)), PBDrawing.Width, Convert.ToSingle(Math.Round(i * PBDrawing.Height / 8)))
+            Next
+            g.DrawLine(New Pen(Color.Blue), 0, -VerticalOffsetBar.Value + PBDrawing.Height, PBDrawing.Width, -VerticalOffsetBar.Value + PBDrawing.Height)
+        End Using
+    End Sub
+
+    'Handles the TimePerDivision Domain
+    Private Sub TimePerDivisionDomain_SelectedItemChanged(sender As Object, e As EventArgs) Handles TimePerDivisionDomain.SelectedItemChanged
+        DrawIterations = 0
+
+        Select Case TimePerDivisionDomain.SelectedItem
+            Case TimePerDivisionDomain.Items(3)
+                Timer1.Interval = 1
+            Case TimePerDivisionDomain.Items(2)
+                Timer1.Interval = 10
+            Case TimePerDivisionDomain.Items(1)
+                Timer1.Interval = 100
+            Case TimePerDivisionDomain.Items(0)
+                Timer1.Interval = 1000
+        End Select
+    End Sub
+
+    'Handles the VoltsPerDivision Domain
+    Private Sub VoltsPerDivisionDomain_SelectedItemChanged(sender As Object, e As EventArgs) Handles VoltsPerDivisionDomain.SelectedItemChanged
+        DrawIterations = 0
+
+        Select Case VoltsPerDivisionDomain.SelectedItem
+            Case VoltsPerDivisionDomain.Items(4)
+                DrawnScale = RNGVal * 10
+            Case VoltsPerDivisionDomain.Items(3)
+                DrawnScale = RNGVal * 2
+            Case VoltsPerDivisionDomain.Items(2)
+                DrawnScale = RNGVal
+            Case VoltsPerDivisionDomain.Items(1)
+                DrawnScale = RNGVal / 2
+            Case VoltsPerDivisionDomain.Items(0)
+                DrawnScale = RNGVal / 5
+        End Select
+
+    End Sub
+
+    'Handles the Vertical Offset Bar scroll
+    Private Sub VerticalOffsetBar_Scroll(sender As Object, e As EventArgs) Handles VerticalOffsetBar.Scroll
+        OffsetTextBox.Text = $"{(VerticalOffsetBar.Value - PBDrawing.Height / 2) / 100} V"
+        If Not Draw Then DrawGrid()
+    End Sub
+
 End Class
+
+
